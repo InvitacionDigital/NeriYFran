5de74c9f4b48ee381d0213ffe762eb8821f2e724
alenmonti <amonti@fi.uba.ar>
2025-11-16 17:51:11 -0300

    tipos arreglo

---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+declare module '*.mp3';
+declare module '*.wav';
+declare module '*.ogg';
+declare module '*.gif';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
