46e2b4e9beb65dedfe16b4f40bbf8f41a65458a0
alenmonti <amonti@fi.uba.ar>
2025-11-21 19:19:58 -0300

    size hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import noscasamos from '/utils/noscasamos.png';
 const Hero: React.FC = () => {
   return (
     <div className='bg-[url("/fondos/bg.jpg")] opacity-95 w-full min-w-[300px] max-w-[1000px] aspect-[9/16] bg-contain bg-center bg-no-repeat flex flex-col justify-center gap-10 py-0 items-center relative '>
-      <img src={noscasamos} alt="noscasamos" className='w-[250px] mx-auto '/>
+      <img src={noscasamos} alt="noscasamos" className='w-[210px] mx-auto '/>
       <div className="absolute inset-0 pointer-events-none">
         <div className="absolute  border border-[3px] border-golden opacity-75" style={{inset: '5rem 2rem 3rem 2rem'}}></div>
       </div>
@@ -14,7 +14,7 @@ const Hero: React.FC = () => {
         <h1 className='-translate-y-5 text-golden'>y</h1>
         <h1 className='text-golden leading-[0.6]'>Fran</h1>
       </div>
-      <div className='flex items-center gap-2 text-2xl text-gray-700 mt-4'>
+      <div className='flex items-center gap-2 text-xl text-gray-700 mt-4'>
         <span className='border-b border-gray-700 w-16 h-0'></span>
         <p className='font-[poppins] text-gray-600'>21.02.2026</p>
         <span className='border-b border-gray-700 w-16 h-0'></span>
